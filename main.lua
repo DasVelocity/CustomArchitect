53783edb3c8397f400d3ac04363fb33c7718961a
Velocity <115943737+DasVelocity@users.noreply.github.com>
2026-08-25 17:27:04 +0200

    Add CustomArchitect initialization in main.lua

---
+++ main.lua
@@ -0,0 +1,13 @@
+local CustomArchitect = loadstring(game:HttpGet("https://raw.githubusercontent.com/DasVelocity/CustomArchitect/refs/heads/main/source.lua"))()
+
+CustomArchitect({
+	CUSTOM_COLOR = "Violet",
+	KILL_MODEL_NAME = "None",
+	MOON_TEXTURE_ID = "rbxassetid://10867606165",
+	MOON_LIGHT_EMISSION = 0.01,
+	HELPFUL_LIGHTEN_FACTOR = 0.5,
+	HINTS = {
+		"You died to a sigma alpha male called Velocity"
+	},
+	HINTS_COLOR = "Blue"
+})
